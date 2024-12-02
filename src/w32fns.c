ff6d06fc85d95cd1f5b95f750bb2bcb9c912424e
Cecilio Pardo <cpardo@imayhem.com>
2024-12-02 17:30:42 +0100

    Send event for key presses and key releases.
    Detect double/tripe taps.
    
    [FIXME: Need adjustments in the description below!]
    
    * src/gtkutil.c (xg_create_frame_widgets): Modified to handle key
    release events.
    (xg_maybe_send_low_level_key_event): New function that sends key
    events.
    (xg_widget_key_press_event_cb): Modified to send low level key events.
    (xg_widget_key_release_event_cb):New function to send low level key
    events on key release.
    * src/keyboard.c (kbd_buffer_get_event): Modified to handle low level
    keyboard events.
    (make_lispy_event): Modified to handle low level keyboard events.
    (kbd_low_level_key_is_enabled): New function to decice if a particular
    key should generate events, looking at configuration,
    (syms_of_keyboard): Added symbols and varialbles.
    (keys_of_keyboard): Modified to map low level key events to
    'special-event-map'.
    * src/keyboard.h: Modified with function prototype.
    * src/pgtkterm.c (pgtk_maybe_send_low_level_key_event): New function
    that sends key events.
    (key_press_event): Modified to handle low level keyboard events.
    (key_release_event): Modified to handle low level keyboard events.
    * src/termhooks.h: Modified to define constant.
    * src/w32fns.c (w32_wnd_proc): Modified to generate low level key
    events.
    * src/w32term.c (w32_read_socket): Modified to generate low level key
    events.
    * src/xterm.c (x_get_modifier_for_keycode): New function to decide
    which (if any) modifier corresponds to a given key.
    (x_maybe_send_low_level_key_event): New function that sends key
    events.
    (handle_one_xevent): Modified to handle low level key events.
    (syms_of_xterm): Modified to definde symbol.
    * src/xterm.h: Modified with function prototype
    * lisp/low-level-key.el (New file).
    (llk-bindings): User bindings for low level key events.
    (llk-tap-count): User option.
    (llk-tap-timeout): User option.
    (llk-tap-keys): User option.
    (llk-keysyms): List of available keysyms.
    (define-xk): Macro for defining keysyms.
    (llk-define-keysyms): Build llk-keysyms.
    (llk-init): Function to initialize low level key handling.
    (event-is-key-press): Get field from event.
    (event-keysym): Get field from event.
    (event-modifier): Get field from event.
    (event-time): Get field from event.
    (llk-bind): Function to create a binding.
    (llk-events): Event history for tap detection.
    (llk-detect-n-tap): Function to detect taps.
    (describe-low-level-key): Command to get information about a key.
    (llk-show-event-description): Show help buffer with information
    about an event.
    (llk-handle): Handler for key events.

--- src/w32fns.c
+++ src/w32fns.c
@@ -4669,10 +4669,10 @@ w32_wnd_proc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     case WM_KEYUP:
     case WM_SYSKEYUP:
       record_keyup (wParam, lParam);
-      if (Venable_low_level_key_events)
+      if (!NILP (Venable_low_level_key_events))
 	{
 	  signal_user_input ();
-	  my_post_msg( &wmsg, hwnd, WM_EMACS_LOW_LEVEL_KEY, wParam, lParam );
+	  my_post_msg (&wmsg, hwnd, WM_EMACS_LOW_LEVEL_KEY, wParam, lParam);
 	}
       goto dflt;
 
@@ -4700,10 +4700,10 @@ w32_wnd_proc (HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
       if (w32_use_fallback_wm_chars_method)
 	wParam = map_keypad_keys (wParam, (lParam & 0x1000000L) != 0);
 
-      if (Venable_low_level_key_events)
+      if (!NILP (Venable_low_level_key_events))
 	{
 	  signal_user_input ();
-	  my_post_msg( &wmsg, hwnd, WM_EMACS_LOW_LEVEL_KEY, wParam, lParam );
+	  my_post_msg (&wmsg, hwnd, WM_EMACS_LOW_LEVEL_KEY, wParam, lParam);
 	}
 
       windows_translate = 0;
